1b92f0922c481f82764e547ce389c7c9b561ac83
haruki7049 <tontonkirikiri@gmail.com>
2026-06-07 23:05:10 +0900

    style: treefmt

--- src/Bigwave.hs
+++ src/Bigwave.hs
@@ -5,7 +5,12 @@ import Data.Word
 
 newtype Samples a = Samples (Array Int a) deriving (Show, Eq)
 
-data Wave a = Wave {waveSamples :: Samples a, waveSampleRate :: Word32, waveChannels :: Word16} deriving (Show, Eq)
+data Wave a = Wave
+  { waveSamples :: Samples a,
+    waveSampleRate :: Word32,
+    waveChannels :: Word16
+  }
+  deriving (Show, Eq)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
